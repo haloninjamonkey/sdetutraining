46a667c5e860dde04850235af972b90cd785c5e7
Patrick Duff <patrick.duff@bodybuilding.com>
2018-10-29 17:14:23 -0600

    Modified HelloWorldApp

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -5,6 +5,9 @@ package basics;
  */
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("I'm adding code to my project");
     }
+
 }
