20ed2a9fbc67665033ab39dba4d9e507fbb36975
Patrick Duff <patrick.duff@bodybuilding.com>
2018-11-01 15:53:33 -0600

    moar learnings

---
+++ src/files/Write.java
@@ -0,0 +1,33 @@
+package files;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by patrick.duff on 11/1/2018.
+ */
+public class Write {
+    public static void main(String[] args) {
+        // 1. Define a path that we want to write
+        String filename = "C:\\Users\\patrick.duff\\Desktop\\SDETuniversity\\Files\\FileToWrite.txt";
+        String message = "I'm writing data that will be placed in a file";
+
+        // 2. Create File in Java
+        File file = new File(filename);
+
+        try {
+            // 3. Open the file
+            FileWriter fw = new FileWriter(file);
+            // 4. Write to a file
+            fw.write(message);
+            // 5. Close the resources
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("ERROR Could not read file: " + filename);
+            e.printStackTrace();
+        } finally {
+            System.out.println("Closing the file writer");
+        }
+
+    }
+}
